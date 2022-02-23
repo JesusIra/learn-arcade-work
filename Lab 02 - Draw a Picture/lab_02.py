93df278432b4ee672bb84548a0dc8544b7480c46
JesusIra <99410789+JesusIra@users.noreply.github.com>
2022-02-23 10:46:53 +0100

    Función de piramides

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -7,23 +7,29 @@ arcade.open_window (800,600, "Drawing example")
 arcade.set_background_color(arcade.color.DARK_ELECTRIC_BLUE)
 arcade.start_render()
 # SOL
-arcade.draw_circle_filled(750, 575, 100, arcade.color.WHITE)
-# ARENA
-arcade.draw_rectangle_filled(400, 0 , 800, 400, arcade.color.GOLDEN_YELLOW)
-arcade.draw_rectangle_filled(400, 0, 800, 300, arcade.color.YELLOW_ROSE)
-# PIRAMIDE DERECHA
-arcade.draw_triangle_filled(0, 100, 300, 100, 150, 400, arcade.color.LIGHT_YELLOW)
-# SOMBRA PIRAMIDE DERECHA
-arcade.draw_triangle_filled(15, 100, 285, 100, 150, 385, arcade.color.YELLOW_ORANGE)
-# PIRAMIDE DERECHA
-arcade.draw_triangle_filled(800, 100, 500, 100, 650, 400, arcade.color.LIGHT_YELLOW)
-# SOMBRA PIRAMIDE DERECHA
-arcade.draw_triangle_filled(785, 100, 515, 100, 650,385, arcade.color.YELLOW_ORANGE)
-# ESTRELLAS
-for _ in range(75):
-    randx= randint(15, 675)
-    randy=randint(400,575)
-    arcade.draw_rectangle_filled(randx, randy, 5, 5, arcade.color.GOLD)
-
+arcade.draw_circle_filled(750, 560, 100, arcade.color.WHITE)
+def dibujar_arena():
+    # ARENA
+    arcade.draw_rectangle_filled(400, 0 , 800, 400, arcade.color.GOLDEN_YELLOW)
+    arcade.draw_rectangle_filled(400, 0, 800, 300, arcade.color.YELLOW_ROSE)
+def dibujar_piramides():
+    #arcade.draw_triangle_filled(x//2,100,x,100, 400, (x//4)*3, arcade.color.LIGHT_YELLOW)
+    # PIRAMIDE DERECHA
+    arcade.draw_triangle_filled(0, 100, 300, 100, 150, 400, arcade.color.LIGHT_YELLOW)
+    # SOMBRA PIRAMIDE DERECHA
+    arcade.draw_triangle_filled(15, 100, 285, 100, 150, 385, arcade.color.YELLOW_ORANGE)
+    # PIRAMIDE DERECHA
+    arcade.draw_triangle_filled(800, 100, 500, 100, 650, 400, arcade.color.LIGHT_YELLOW)
+    # SOMBRA PIRAMIDE DERECHA
+    arcade.draw_triangle_filled(785, 100, 515, 100, 650,385, arcade.color.YELLOW_ORANGE)
+def dibujar_estrellas():
+    # ESTRELLAS
+    for _ in range(75):
+        randx= randint(15, 650)
+        randy=randint(400,575)
+        arcade.draw_rectangle_filled(randx, randy, 5, 5, arcade.color.GOLD)
+dibujar_arena()
+dibujar_piramides()
+dibujar_estrellas()
 arcade.finish_render()
 arcade.run()
